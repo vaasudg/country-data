20ab9b6cdecaed985e8bd606c3e3d3141f6b324b
Vasu <enf71471@adobe.com>
2024-11-23 02:15:46 +0530

    workflow updated, lint config updated

--- frontend/src/app/app.tsx
+++ frontend/src/app/app.tsx
@@ -1,14 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
 
-import NxWelcome from './nx-welcome';
 
 import { Route, Routes, Link } from 'react-router-dom';
 
 export function App() {
   return (
     <div>
-      <NxWelcome title="frontend" />
 
       {/* START: routes */}
       {/* These routes and navigation have been generated for you */}
